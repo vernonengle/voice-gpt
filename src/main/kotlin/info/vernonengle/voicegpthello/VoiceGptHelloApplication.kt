7e75ea56a0cc09d22e551f64dea1ca16113c0f6f
vernonengle <vernon.g.engle@gmail.com>
2023-08-10 14:58:31 +1000

    initial commit

---
+++ src/main/kotlin/info/vernonengle/voicegpthello/VoiceGptHelloApplication.kt
@@ -0,0 +1,11 @@
+package info.vernonengle.voicegpthello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoiceGptHelloApplication
+
+fun main(args: Array<String>) {
+    runApplication<VoiceGptHelloApplication>(*args)
+}
---
+++ src/test/kotlin/info/vernonengle/voicegpthello/VoiceGptHelloApplicationTests.kt
@@ -0,0 +1,13 @@
+package info.vernonengle.voicegpthello
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoiceGptHelloApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
